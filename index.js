fd4457babfb7b5a79818dbfc323073b2f42c55b2
seasons986 <annalin12345@gmail.com>
2023-08-10 12:47:39 -0400

    interesting filled grid generation

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
 
+  let rows = [0,1,2,3];
+  const grid = document.getElementById("grid");
+
+  function initGrid(words, rowId) {
+    const tr = document.createElement("tr");
+    tr.id = rowId;
+    const randomKey = Object.keys(words)[getRandomInt(0, Object.keys(words).length)];
+    const word = words[randomKey][getRandomInt(0, words[randomKey].length)];
+    word.split("").forEach((letter) => {
+      const td = document.createElement("td");
+      td.innerHTML = letter;
+      tr.appendChild(td);
+    })
+    grid.appendChild(tr);
+  }
+
   // 4-letter words list scraped from https://eslforums.com/4-letter-words/
   fetch("words.json").then((f) => f.json()).then((r) => {
-    console.log(r);
-  })
+    rows.forEach((row) => initGrid(r, row));
+  });
 });
